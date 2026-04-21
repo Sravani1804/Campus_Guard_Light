ae2b12b388b7c386bbda2300478c9d2d99f613e9
Sravani1804 <151452361+Sravani1804@users.noreply.github.com>
2026-04-21 12:08:03 +0530

    Update similarity.py

--- backend/lost_found_ai/similarity.py
+++ backend/lost_found_ai/similarity.py
@@ -1,4 +1,10 @@
-from sklearn.metrics.pairwise import cosine_similarity
+import cv2
 
-def compute_similarity(emb1, emb2):
-    return cosine_similarity(emb1, emb2)[0][0]
+def compute_similarity(desc1, desc2):
+    if desc1 is None or desc2 is None:
+        return 0
+
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    matches = bf.match(desc1, desc2)
+
+    return len(matches)
