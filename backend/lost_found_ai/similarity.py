32e9214f4d66ccab0228d2bdc844083b0ff1135a
Sravani1804 <151452361+Sravani1804@users.noreply.github.com>
2026-04-22 20:04:54 +0530

    Update similarity.py

--- backend/lost_found_ai/similarity.py
+++ backend/lost_found_ai/similarity.py
@@ -4,7 +4,13 @@ def compute_similarity(desc1, desc2):
     if desc1 is None or desc2 is None:
         return 0
 
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-    matches = bf.match(desc1, desc2)
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING)
 
-    return len(matches)
+    matches = bf.knnMatch(desc1, desc2, k=2)
+
+    good = []
+    for m, n in matches:
+        if m.distance < 0.75 * n.distance:
+            good.append(m)
+
+    return len(good)
